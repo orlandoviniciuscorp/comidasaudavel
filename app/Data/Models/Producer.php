d49516345d7ba4b283833c12cf9a676708c6babc
Orlando Vinícius <orlando.vinicius1987@gmail.com>
2019-08-17 01:57:06 +0000

    Issue: #1

---
+++ app/Data/Models/Producer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Data\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Producer extends Model
+{
+    //
+
+    protected $table;
+
+
+    protected $fillable = ['name','description'];
+
+}
